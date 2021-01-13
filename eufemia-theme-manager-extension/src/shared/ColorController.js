bc6a856b4ca2f69b4a3c481ad0be13c554ac64da
Tobias Høegh <tobias@tujo.no>
2021-01-13 21:39:01 +0100

    chore: refactor filter

--- eufemia-theme-manager-extension/src/shared/ColorController.js
+++ eufemia-theme-manager-extension/src/shared/ColorController.js
@@ -58,32 +58,3 @@ export const generateThemeIgnoreColors = () =>
       return `${key}: ${value};`
     })
     .join('\n')
-
-export function applyFilter(filter, list) {
-  if (filter) {
-    if (filter?.value?.length > 0) {
-      const endsWithWhiteSpace = /\s$/.test(filter.value)
-      const listOfSearchValues = filter.value.split(/\s+/g)
-      list = list.filter(({ key }) => {
-        if (listOfSearchValues.length > 0) {
-          return listOfSearchValues.some((item) => {
-            return key.includes(item)
-          })
-        } else {
-          if (endsWithWhiteSpace) {
-            return new RegExp(`.*${filter.value.trim()}$`).test(key)
-          }
-          return key.includes(filter.value.trim())
-        }
-      })
-    }
-
-    if (filter?.active) {
-      list = list.filter(({ change }) => {
-        return change
-      })
-    }
-  }
-
-  return list
-}
