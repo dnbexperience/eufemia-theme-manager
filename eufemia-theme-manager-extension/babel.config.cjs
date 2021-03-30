baca48a0c93dbcc99a14ff564f594c3de190a449
Tobias Høegh <tobias@tujo.no>
2021-03-30 07:50:14 +0200

    Use newer babel browser target

--- eufemia-theme-manager-extension/babel.config.cjs
+++ eufemia-theme-manager-extension/babel.config.cjs
@@ -5,7 +5,14 @@
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: 'defaults' }],
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          chrome: '89',
+        },
+      },
+    ],
     ['@babel/preset-react'],
   ],
 }
